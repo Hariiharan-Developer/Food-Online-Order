71ada20a6c05d440c98732a8c7ac1d79bb03376b
Hariharan.s <hreehs1428@gmail.com>
2025-08-25 00:14:40 +0530

    React-context

--- Frontent/src/components/header/Header.jsx
+++ Frontent/src/components/header/Header.jsx
@@ -4,8 +4,9 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-content">
-        <h2>Order your favourite foods herr</h2>
+        <h2>Order your favourite foods here</h2>
         <p>Our Food Order App makes dining simple and convenient. Users can browse restaurants, explore menus, and place orders with just a few taps. From breakfast to late-night cravings, the app connects customers with their favorite dishes quickly and securely.</p>
+        <button>View Menu</button>
       </div>
     </div>
   )
--- Frontent/src/components/pages/home/Home.jsx
+++ Frontent/src/components/pages/home/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../header/Header'
+import ExploreMenu from '../../exploreMenu/ExploreMenu'
+import FoodDisplay from '../../foodDisplay/FoodDisplay'
 const Home = () => {
+
+  const [category,setCategory]=useState('All')
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+      
     </div>
   )
 }
