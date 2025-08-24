b28025e09163278cb7fa29ce5f6a4bd6e232b029
Hariharan.s <hreehs1428@gmail.com>
2025-08-24 20:40:35 +0530

    Routes Created

---
+++ Frontent/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your favourite foods herr</h2>
+        <p>Our Food Order App makes dining simple and convenient. Users can browse restaurants, explore menus, and place orders with just a few taps. From breakfast to late-night cravings, the app connects customers with their favorite dishes quickly and securely.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- Frontent/src/components/pages/home/Home.jsx
+++ Frontent/src/components/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../header/Header'
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
