216b35998bb58b997b03d14f8cdf6809f78b7d2a
Hariharan.s <hreehs1428@gmail.com>
2025-08-26 00:38:08 +0530

    database connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors =require('cors')
+const dotenv = require('dotenv').config()
+const connectDb = require('./config/db')
 
 
 //app config:
@@ -10,6 +12,7 @@ const port=4000
 app.use(express.json())
 app.use(cors())
 
+connectDb()
 app.get('/',(req,res)=>{
     res.send('API Working')
 })
