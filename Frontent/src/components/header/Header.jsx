3499782479b76db7dcfccddf65c9882a59efd8c2
Hariharan.s <hreehs1428@gmail.com>
2025-08-30 11:14:28 +0530

    Update Header.jsx

--- Frontent/src/components/header/Header.jsx
+++ Frontent/src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import './Header.css'
+import './header.css'
 import { assets } from '../../assets/assets'   // make sure path is correct
 
 const Header = () => {
