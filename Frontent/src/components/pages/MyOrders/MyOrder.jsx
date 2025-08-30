369bf1b0645d79a3bc66bdf9e6d0b18486a5c181
Hariharan.s <hreehs1428@gmail.com>
2025-08-30 10:58:50 +0530

    typo fixed in myorder

--- Frontent/src/components/pages/MyOrders/MyOrder.jsx
+++ Frontent/src/components/pages/MyOrders/MyOrder.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { StoreContext } from '../../../context/StoreContext'
 import axios from 'axios'
 import { assets } from '../../../assets/assets'
-import './Myorder.css'
+import './MyOrder.css'
 
 const MyOrder = () => {
     const {url,token} =useContext(StoreContext)
