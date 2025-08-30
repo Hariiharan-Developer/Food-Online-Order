bb876541005728361a1e5063e7ba351c8e878cf1
Hariharan.s <hreehs1428@gmail.com>
2025-08-30 14:16:15 +0530

    Updated

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App=()=>{
-  const url = 'https://food-online-order-frontend.onrender.com'
+  const url = 'https://food-online-order-kazs.onrender.com'
   return(
   <div>
     <ToastContainer/>
